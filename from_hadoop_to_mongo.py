d31e403b396b1ca24e6d532d7ff27b45de94f5bb
Cyril Vincent <contact@cyrilvincent.com>
2025-07-22 11:12:03 +0200

    ppt j-3

--- from_hadoop_to_mongo.py
+++ from_hadoop_to_mongo.py
@@ -9,12 +9,12 @@ with client.read("/house/house.csv") as f:
     reader = csv.DictReader(io.StringIO(f.read().decode('utf-8')))
     l = []
     for row in reader:
-        json = {"loyer":float(row["loyer"]),"surface":float(row["surface"])}
+        json = {"loyer": float(row["loyer"]),"surface": float(row["surface"])}
         l.append(json)
     print(l)
 
 with pymongo.MongoClient('localhost', 27017) as client:
-    db = client.formation # database created with MongoDbCompass
+    db = client.local
     db.create_collection("house_from_hadoop")
     result = db.house.insert_many(l)
 print(result.inserted_ids)
