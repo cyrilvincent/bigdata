2d9d1d677841ad2cd46aeee197d4bedbbf90dd4e
Cyril Vincent <contact@cyrilvincent.com>
2025-09-09 15:58:29 +0200

    BigQuery & demo_cancer

--- demo_tf.py
+++ demo_tf.py
@@ -9,7 +9,7 @@ from tensorflow.keras.applications.resnet_v2 import ResNet152V2
 model = ResNet152V2()
 
 # load an image from file
-image = load_img("data/dogsvscats/small/validation/dogs/dog.1000.jpg", target_size=(224, 224))
+image = load_img("data/dogsvscats/small/validation/cats/cat.1000.jpg", target_size=(224, 224))
 # convert the image pixels to a numpy array
 image = img_to_array(image)
 # reshape data for the model : (nbsample, length, width, colordepth)
